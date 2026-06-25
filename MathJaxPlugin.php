2a80b27698c45a64f7fa363e246af69b92337ee5
Joe Simpson <joe@invisibledragonltd.com>
2026-06-25 21:19:56 +0100

    Implement inline MathJax

--- MathJaxPlugin.php
+++ MathJaxPlugin.php
@@ -64,20 +64,11 @@ class MathJaxPlugin extends GenericPlugin {
     }
 
     public function renderMathJax(string $input) {
-        $re = '/&lt;math.+&lt;\/math&gt;/m';
+        // math tags after being html encoded
+        $re = '/&lt;(mml:)?math.+&lt;\/(mml:)?math&gt;/msU';
 
         $input = preg_replace_callback($re, function($matches) {
-            if(!$this->hasMathJax) {
-                $request = Application::get()->getRequest();
-                $templateMgr = TemplateManager::getManager($request);
-                $MathJaxScript = '</script><script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@4/tex-mml-chtml.js">';
-                $templateMgr->addJavaScript('mathjax', $MathJaxScript, 
-                    array(
-                        'inline' => true,
-                        'contexts' => array('frontend', 'backend')
-                    )
-                );
-            }
+            $this->addMathJax();
             return strip_tags( html_entity_decode($matches[0]), [
                 'math', 'mi', 'mo', 'mfrac', 'mrow', 'msqrt', 'mn',
                 'munder', 'munderover', 'mtr', 'mtext', 'mtd', 'mtable',
@@ -87,7 +78,48 @@ class MathJaxPlugin extends GenericPlugin {
             ] );
         }, $input);
 
+        // Inline Math attempt
+        $re = '/[\$\[\(].+[\$\]\)]/m';
+        if(preg_match($re, $input)) {
+            $this->addMathJax();
+        }
+
+        $input = '<div class="tex2jax_process">' . $input . '</div>';
+
         return $input;
+    }
+
+    public function addMathJax() {
+        if(!$this->hasMathJax) {
+            $request = Application::get()->getRequest();
+            $templateMgr = TemplateManager::getManager($request);
+            $MathJaxScript = <<<EOF
+window.MathJax = {
+  startup: {
+    elements: ['div.tex2jax_process'],
+  },
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    tags: 'ams'
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  }
+};
+</script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@4/tex-mml-chtml.js">
+EOF;
+            $templateMgr->addJavaScript('mathjax', $MathJaxScript, 
+                array(
+                    'inline' => true,
+                    'contexts' => array('frontend', 'backend')
+                )
+            );
+        }
     }
 
 }
